60e5cb9a58dff62df27e24fa8cbe44ef0bd5c676
Robert Maefs <>
2024-01-13 12:15:46 -0500

    trying to merge bbox to fix ordinality of the list-items

--- marker/cleaners/lists.py
+++ marker/cleaners/lists.py
@@ -35,6 +35,13 @@ def merge_list_blocks(blocks: List[Page]):
                     current_bbox = None
 
                 new_page_blocks.append(block)
+
+                for line in block.lines:
+                    for span in line.spans:
+                        if span.text.strip():
+                            print(f"[ypos: {str(span.bbox[1])[:3]},{str(span.bbox[3])[:3]}], Text: {span.text}")
+
+                
                 continue
 
             ## begin working on data that are list items
@@ -51,13 +58,15 @@ def merge_list_blocks(blocks: List[Page]):
                 for span in line.spans:
                     trimmed_text = span.text.strip()
                     xpos = math.floor(span.bbox[0])
+                    ypos = math.floor(span.bbox[1])
+                    ypos2 = math.floor(span.bbox[3])
                     indent = math.floor((xpos - 48) / 18)
                     
                     if is_list_item_indicator(trimmed_text):
                         if current_list_item_span is not None:
                             ## since we're starting a new list item
                             ## and we already have on in the loop
-                            ## write it out to whereever it will get to the output
+                            ## write it out to where ever it will get to the output
                             ## then make a new one
                             ## Probably this means adding a new block with new lines with this span
                             ## but i dont' know where to put it or how to get it to interleave with
@@ -70,11 +79,11 @@ def merge_list_blocks(blocks: List[Page]):
                             ## as the start of the list-item 
                             ## printing the output looks perfect, but there's some magical something that occurs after this
                             ## that reorders the items on the page mysteriously
-                            ##current_lines.append(Line(spans=[current_list_item_span], bbox=current_list_item_span.bbox))
+                            current_lines.append(Line(spans=[current_list_item_span], bbox=current_list_item_span.bbox))
 
 
                         ind = "\t" * indent
-                        text = f" {ind}[{xpos}-{indent}]{span.text.strip()}"
+                        text = f" {ind}[ypos: {ypos},{ypos2}]{span.text.strip()}"
                         current_list_item_span = Span(
                                                     bbox=span.bbox,
                                                     span_id=span.span_id,
@@ -88,9 +97,10 @@ def merge_list_blocks(blocks: List[Page]):
                         if current_list_item_span is not None:
                             # Append text to the current list item span
                             current_list_item_span.text += " " + span.text.strip()
+                            current_list_item_span.bbox = merge_boxes(current_list_item_span.bbox, span.bbox)
                             span.text = "" #preferably delete this but i don't know what that will do to the schema
                 #perferably delete empty lines but i don't know what that will do to the schema
-            #note, the current_list_item spans blocks, and probably needs to span pages so this whole loop nesting process might need another layer                        
+                #note, the current_list_item spans blocks, and probably needs to span pages so this whole loop nesting process might need another layer                        
 
         if len(current_lines) > 0:
             new_block = Block(
