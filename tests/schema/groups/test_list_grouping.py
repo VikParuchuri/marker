20fec484d67d7744a855f291fa3bfe6796fd699a
Moses Paul R <iammosespaulr@gmail.com>
2024-11-19 12:31:14 +0000

    fix typo [skip ci]

--- tests/schema/groups/test_list_grouping.py
+++ tests/schema/groups/test_list_grouping.py
@@ -9,11 +9,10 @@ def test_list_grouping(pdf_document):
     structure = StructureBuilder()
     structure(pdf_document)
 
-    page = pdf_document.pags[0]
+    page = pdf_document.pages[0]
     list_groups = []
     for block in page.children:
         if block.block_type == BlockTypes.ListGroup:
             list_groups.append(block)
 
     assert len(list_groups) == 1
-
