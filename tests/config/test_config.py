54339d99ac3850026cf58ced8a2f852e0c45f1db
Vik Paruchuri <vik.paruchuri@gmail.com>
2025-01-24 12:12:07 -0500

    Add additional test

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -1,13 +1,13 @@
 import sys
 from contextlib import suppress
-from marker.config.parser import ConfigParser
-
 import click
 
 from marker.config.printer import CustomClickPrinter
+from marker.config.crawler import crawler
+from marker.config.parser import ConfigParser
 
 
-def test_config_parser():
+def capture_kwargs(argv):
     command = click.command(cls=CustomClickPrinter)
     captured_kwargs = {}
 
@@ -16,21 +16,32 @@ def test_config_parser():
         return kwargs
 
     original_argv = sys.argv
-    sys.argv = ['test', '--disable_multiprocessing', '--output_dir', 'output_dir', "--height_tolerance", "0.5"]
+    sys.argv = argv
     try:
         with suppress(SystemExit):
             command(ConfigParser.common_options(parse_args))()
     finally:
         sys.argv = original_argv
 
-    kwargs = captured_kwargs
+    return captured_kwargs
+
+
+def test_config_parser():
+    sys.argv = ['test', '--disable_multiprocessing', '--output_dir', 'output_dir', "--height_tolerance", "0.5"]
+    kwargs = capture_kwargs(sys.argv)
     parser = ConfigParser(kwargs)
     config_dict = parser.generate_config_dict()
 
     # Validate kwarg capturing
-    assert captured_kwargs["disable_multiprocessing"] == True
-    assert captured_kwargs["output_dir"] == "output_dir"
+    assert kwargs["disable_multiprocessing"] == True
+    assert kwargs["output_dir"] == "output_dir"
 
     assert config_dict["pdftext_workers"] == 1 # disabling multiprocessing does this
     assert config_dict["height_tolerance"] == 0.5
     assert "output_dir" not in config_dict # This is not a config key
+
+def test_config_none():
+    kwargs = capture_kwargs(['test'])
+
+    for key in crawler.attr_set:
+        assert kwargs.get(key) is None
