be2aeec7a1721e87592c523fbb15e613197dad46
Tarun Menta <menta.tarun@gmail.com>
2025-03-13 11:16:13 -0400

    Fix tags

--- marker/builders/ocr.py
+++ marker/builders/ocr.py
@@ -135,7 +135,7 @@ class OcrBuilder(BaseBuilder):
             is_closing_tag, format = get_closing_tag_type(char.text)
             if is_closing_tag:
                 formats.remove(format)
-                if current_span and format=='math':
+                if current_span:
                     if format == 'math':
                         current_span.html = f'<math display="inline">{current_span.text}</math>'
                     spans.append(current_span)
