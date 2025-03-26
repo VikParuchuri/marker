9612bdc59b937150095d249f471629a9a224f7be
Vik Paruchuri <vik.paruchuri@gmail.com>
2025-03-26 13:42:25 -0400

    Add pre-commit hook

--- marker/providers/registry.py
+++ marker/providers/registry.py
@@ -12,6 +12,27 @@ from marker.providers.powerpoint import PowerPointProvider
 from marker.providers.spreadsheet import SpreadSheetProvider
 
 
+def provider_from_ext(filepath: str):
+    ext = filepath.split(".")[-1].strip()
+    if not ext:
+        return PdfProvider
+
+    if ext in ["jpg", "jpeg", "png", "gif", "webp"]:
+        return ImageProvider
+    if ext in ["pdf"]:
+        return PdfProvider
+    if ext in ["doc", "docx", "odt"]:
+        return DocumentProvider
+    if ext in ["xls", "xlsx", "ods"]:
+        return SpreadSheetProvider
+    if ext in ["ppt", "pptx", "odp"]:
+        return PowerPointProvider
+    if ext in ["epub"]:
+        return EpubProvider
+
+    return PdfProvider
+
+
 def provider_from_filepath(filepath: str):
     if filetype.image_match(filepath) is not None:
         return ImageProvider
@@ -19,34 +40,40 @@ def provider_from_filepath(filepath: str):
         return PdfProvider
     if match(filepath, (archive.Epub(),)) is not None:
         return EpubProvider
-    if match(
-            filepath, (
-                document.Doc(),
-                document.Docx(),
-                document.Odt()
-            )) is not None:
+    if match(filepath, (document.Doc(), document.Docx(), document.Odt())) is not None:
         return DocumentProvider
-    if match(
-            filepath, (
+    if (
+        match(
+            filepath,
+            (
                 document.Xls(),
                 document.Xlsx(),
                 document.Ods(),
-            )) is not None:
+            ),
+        )
+        is not None
+    ):
         return SpreadSheetProvider
-    if match(
-            filepath, (
+    if (
+        match(
+            filepath,
+            (
                 document.Ppt(),
                 document.Pptx(),
                 document.Odp(),
-            )) is not None:
+            ),
+        )
+        is not None
+    ):
         return PowerPointProvider
 
     try:
-        soup = BeautifulSoup(open(filepath, 'r').read(), 'html.parser')
+        soup = BeautifulSoup(open(filepath, "r").read(), "html.parser")
         # Check if there are any HTML tags
         if bool(soup.find()):
             return HTMLProvider
-    except:
+    except Exception:
         pass
 
-    return PdfProvider
+    # Fallback if we incorrectly detect the file type
+    return provider_from_ext(filepath)
