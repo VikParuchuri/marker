42d3aeba81baad99b617746de705254997dba388
Vik Paruchuri <vik.paruchuri@gmail.com>
2025-05-07 12:13:54 -0400

    Remove global logging change

--- marker/logger.py
+++ marker/logger.py
@@ -3,11 +3,9 @@ import warnings
 
 
 def configure_logging():
-    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger("PIL").setLevel(logging.ERROR)
+    warnings.simplefilter(action="ignore", category=FutureWarning)
 
-    logging.getLogger('PIL').setLevel(logging.ERROR)
-    warnings.simplefilter(action='ignore', category=FutureWarning)
-
-    logging.getLogger('fontTools.subset').setLevel(logging.ERROR)
-    logging.getLogger('fontTools.ttLib.ttFont').setLevel(logging.ERROR)
-    logging.getLogger('weasyprint').setLevel(logging.CRITICAL)
+    logging.getLogger("fontTools.subset").setLevel(logging.ERROR)
+    logging.getLogger("fontTools.ttLib.ttFont").setLevel(logging.ERROR)
+    logging.getLogger("weasyprint").setLevel(logging.CRITICAL)
